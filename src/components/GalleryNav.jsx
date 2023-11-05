1395f0f5881a60c6fa595c688d928a85a36018f4
usupal <udyan.upal@gmail.com>
2023-11-05 18:05:20 +0600

    🎉 feat(GalleryNav.jsx): add GalleryNav component to display a navigation bar for the gallery page
    The GalleryNav component is responsible for rendering the navigation bar at the top of the gallery page. It includes a title, which is "Gallery" by default, but changes to show the number of selected images when there are selected images. It also includes a "Delete files" button to delete the selected files.
    
    This commit adds the GalleryNav component to the project.

---
+++ src/components/GalleryNav.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const GalleryNav = ({ selectImages, setSelectImages, handleDeleteFile }) => {
+  return (
+    <nav className="py-4 px-6 border-b-2">
+      <article className="flex flex-row justify-between items-center">
+        <h1 className="text-2xl font-bold ">
+          {selectImages.length === 0 ? (
+            <span className="font-bold text-2xl ">Gallery</span>
+          ) : (
+            <label htmlFor="select" className="flex flex-row justify-between items-center gap-x-4">
+              <input
+                type="checkbox"
+                name="select"
+                id="select"
+                checked={selectImages.length > 0}
+                className="w-5 h-5 to-blue-600 cursor-pointer"
+                onChange={() => setSelectImages([])}
+              />
+              {selectImages.length} Files Selected
+            </label>
+          )}
+        </h1>
+        <button
+          className="text-red-500 font-medium hover:scale-105 transform transition-transform hover:duration-300 p-2 rounded-lg hover:underline flex items-center justify-center"
+          onClick={handleDeleteFile}
+        >
+          Delete files
+        </button>
+      </article>
+    </nav>
+  );
+};
+
+export default GalleryNav;
